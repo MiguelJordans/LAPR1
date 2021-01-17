20dfac6d1a4a0ce4eb59dfdf2c08fe1477ae38e1
AzureAD\MiguelJordão(1201487 <1201487@isep.ipp.pt>
2021-01-17 21:03:23 +0000

    Update às classes ( mais info dentro do "info.txt" ) ( Meti estas classes numa nova pasta para se fazer testes no dia 18/01 )

---
+++ src/Testar/Distribuição.java
@@ -0,0 +1,230 @@
+public class Distribuição {
+
+    public static void ApresentarDist(double[][] matriz, int k, double[] X){
+
+
+        double[][] matrizGeraçao;
+        double[] produto;
+        double dimensão;
+        double[] vetorNormalizado;
+        double[][] matrizGeraçao_1;
+        double[] produto_1;
+        double[] guardaTaxa = new double[200];
+        double[] guardaDimensão = new double[200];
+        int t = 0;
+        int counter = 0;
+        int i = 0;
+
+        System.out.println("*****************************************");
+        System.out.println();
+
+        while (t <= k) {
+
+            System.out.printf("Para a %dª geração:\n\n", t + 1);
+
+            if (t>1) {
+
+                matrizGeraçao = Cálculos.leslieT(matriz, t);
+                matrizGeraçao_1 = Cálculos.leslieT(matriz, t + 1);
+
+                produto = Cálculos.multiplicarMatrizporVetor(matrizGeraçao, X);
+                produto_1 = Cálculos.multiplicarMatrizporVetor(matrizGeraçao_1, X);
+
+                dimensão = dimensao(produto);
+
+                vetorNormalizado = distribuiçaoNormalizada(produto, dimensão);
+
+                System.out.println("Distruibuição não Normalizada:");
+                Apresentar.apresentarVetor(produto);
+
+                System.out.println("Distribuição Normalizada:");
+                Apresentar.apresentarVetor(vetorNormalizado);
+
+                System.out.print("Dimensão da população nessa geração= ");
+                System.out.printf("%.2f\n", dimensão);
+
+                System.out.println();
+
+                System.out.printf("T(%d)/", t + 1);
+                escreverTaxaVariacao(produto, t, produto_1, guardaTaxa, counter);
+                counter++;
+
+                t++;
+
+                guardarDimensão(guardaDimensão,dimensão,i);
+                i++;
+
+                System.out.println();
+                System.out.println("*****************************************");
+                System.out.println();
+
+            }
+
+
+            else if(t==1){
+
+                matrizGeraçao = Cálculos.leslieT(matriz, t-1);
+                matrizGeraçao_1 = Cálculos.leslieT(matriz, t);
+
+                produto = Cálculos.multiplicarMatrizporVetor(matrizGeraçao, X);
+                produto_1 = Cálculos.multiplicarMatrizporVetor(matrizGeraçao_1, X);
+
+                dimensão = dimensao(produto);
+
+                vetorNormalizado = distribuiçaoNormalizada(produto, dimensão);
+
+                System.out.println("Distruibuição não Normalizada:");
+                Apresentar.apresentarVetor(produto);
+
+                System.out.println("Distribuição Normalizada:");
+                Apresentar.apresentarVetor(vetorNormalizado);
+
+                System.out.print("Dimensão da população nessa geração= ");
+                System.out.printf("%.2f\n", dimensão);
+
+                System.out.println();
+
+                System.out.printf("T(%d)/", t + 1);
+                escreverTaxaVariacao(produto, t, produto_1, guardaTaxa, counter);
+                counter++;
+
+                t++;
+
+                guardarDimensão(guardaDimensão,dimensão,i);
+                i++;
+
+                System.out.println();
+                System.out.println("*****************************************");
+                System.out.println();
+
+            }
+
+              else {
+
+                matrizGeraçao_1 = Cálculos.leslieT(matriz, t + 1);
+
+                produto = X;
+                produto_1 = Cálculos.multiplicarMatrizporVetor(matrizGeraçao_1, X);
+
+                dimensão = dimensao(produto);
+
+                vetorNormalizado = distribuiçaoNormalizada(produto, dimensão);
+
+                System.out.println("Distruibuição não Normalizada:");
+                Apresentar.apresentarVetor(produto);
+
+                System.out.println("Distribuição Normalizada:");
+                Apresentar.apresentarVetor(vetorNormalizado);
+
+                System.out.print("Dimensão da população nessa geração= ");
+                System.out.printf("%.2f\n", dimensão);
+
+                System.out.println();
+
+                System.out.printf("T(%d)/", t + 1);
+                escreverTaxaVariacao(produto, t, produto_1, guardaTaxa, counter);
+                counter++;
+
+                t++;
+
+                guardarDimensão(guardaDimensão,dimensão,i);
+                i++;
+
+
+                System.out.println();
+                System.out.println("*****************************************");
+                System.out.println();
+
+
+            }
+
+        }
+
+        Vectores.vetores(matriz);
+
+        System.out.println("Numero total de individuos");
+        System.out.printf("(t ; delta_t)\n");
+        Apresentar.apresentarTaxaOUDimensão(guardaTaxa);
+
+        System.out.println("Crescimento da população");
+        System.out.printf("(t ; Nt)\n");
+       Apresentar.apresentarTaxaOUDimensão(guardaDimensão);
+
+    }
+
+
+    public static double dimensao(double[] produto) {
+
+        double soma = 0;
+
+        for (int j = 0; j < produto.length; j++) {
+
+            soma += produto[j];
+
+
+        }
+
+        return soma;
+
+    }
+
+
+    public static double[] distribuiçaoNormalizada(double[] produto, double dimensao) {
+
+
+        double[] vetorNormalizado = new double[produto.length];
+
+
+
+        for (int j = 0; j < produto.length; j++) {
+
+            vetorNormalizado[j] = (produto[j] / dimensao) * 100;
+
+        }
+
+        return vetorNormalizado;
+
+    }
+
+
+    public static void escreverTaxaVariacao(double[] produto, int k,double []produto_1,double[]guardaTaxa,int counter) {
+
+
+
+        System.out.print("Taxa de variação=");
+
+        double taxaVariacao = taxaVariacao(produto,k,produto_1);
+
+        System.out.printf("%.2f\n",taxaVariacao);
+
+        guardaTaxa[counter]=taxaVariacao;
+
+
+    }
+
+
+    public static double taxaVariacao(double[] produto, int k,double[]produto_1) {
+
+
+        double Nt;
+        double Nt1;
+        double taxa;
+
+        Nt1 = dimensao(produto_1);
+        Nt = dimensao(produto);
+
+        taxa = Nt1 / Nt;
+
+        return taxa;
+
+
+    }
+
+
+    public static void guardarDimensão(double[]guardarDimensão, double dimensão,int i) {
+
+        guardarDimensão[i]=dimensão;
+
+    }
+
+}
---
+++ src/Testar/Vectores.java
@@ -0,0 +1,87 @@
+import org.la4j.Matrix;
+import org.la4j.matrix.dense.Basic2DMatrix;
+import org.la4j.decomposition.EigenDecompositor;
+
+public class Vectores {
+
+    public static void vetores(double[][] firstMatrix) {
+
+        double mvalorp;
+
+        mvalorp = vetorproprio(firstMatrix);
+
+        System.out.printf("-lambda=%.4f\n", mvalorp);
+
+        comportamentoAssintotico(mvalorp);
+        System.out.println();
+
+    }
+
+    public static double vetorproprio(double[][] matriz) {
+
+        double lambda;
+
+        Matrix a = new Basic2DMatrix(matriz);
+
+        EigenDecompositor eigenD = new EigenDecompositor(a);
+        Matrix[] mattD = eigenD.decompose();
+
+        double[][] matA = mattD[0].toDenseMatrix().toArray(); //matA - vetor proprio
+        double[][] matB = mattD[1].toDenseMatrix().toArray(); //matB - valor proprio
+
+        System.out.println("Vetor Próprio Bidimensional (Estudar para corrigir os valores)");
+        Apresentar.apresentarMatriz(matA);
+
+        System.out.printf("Vetor próprio:");
+
+        Apresentar.apresentarVetorBidimensional(matriz,matA);
+
+        lambda = maiorValorProprio(matB);
+
+        return lambda;
+
+    }
+
+    public static double maiorValorProprio(double[][] matB) {
+
+        //verifica qual é o modulo do maior valor e guarda o número
+
+        double maior = matB[0][0];
+
+        for (int i = 0; i < matB.length; i++) {
+
+            if (Math.abs(matB[i][i]) > maior) {
+
+                maior = matB[i][i];
+
+
+            }
+
+        }
+
+        return maior;
+
+    }
+
+
+    public static void comportamentoAssintotico(double numero) {
+
+        System.out.println();
+
+        if (numero > 1) {
+
+            System.out.println("-A população tenderá para +infinito");
+
+        } else if (numero == 1) {
+
+            System.out.println("-A população permanecerá constante");
+
+        } else {
+
+            System.out.println("-A população tenderá para 0");
+
+        }
+
+    }
+
+}
