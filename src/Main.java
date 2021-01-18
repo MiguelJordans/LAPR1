b24f13a474f4d8cce4258bcb58318ea931963410
AzureAD\MiguelJordão(1201487 <1201487@isep.ipp.pt>
2021-01-18 11:36:30 +0000

    Update ao main ( tinha erros )

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
 
-<<<<<<< HEAD
-    public static void main(String[] args) {
-=======
-        static Scanner read = new Scanner(System.in);
+    static Scanner read = new Scanner(System.in);
 
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
             if (args.length != 0) {
 
@@ -13,7 +12,6 @@ public class Main {
                 try {
 
                     {
-
                         String nomeFicheiro = "";
                         String nomeFicheiroSaida = "";
                         String numGeracoes = "";
@@ -71,12 +69,14 @@ public class Main {
                                         System.exit(0);
                                     }
                                     break;
+
                                 //Calcular valor e vector próprio
                                 case "-e": vecProprio = true; break;
                                 //Calcular dimensão população a cada geração
                                 case "-v": dimPopulacao = true; break;
                                 //Calcular variação da População entre gerações
                                 case "-r": varPopGeracoes = true; break;
+
                             }
                         }
 
@@ -97,63 +97,6 @@ public class Main {
                     }
 
 
-                } catch (Exception e) {
-
-                    e.printStackTrace();
-
-                }
-
-            } else {
-
-                boolean choice = true;
-                int flag=0;
-
-                while (choice) {
-
-                    String choi = "";
-
-                    double[] taxaS = new double[199];
-                    double[] taxaF = new double[199];
-                    double[] nIndividuos = new double[199];
-                    int t;
-
-<<<<<<< HEAD
-                    Welcome.Welcome();
-
-                    System.out.print("Por favor introduza a matriz de Lelie:\n\n");
-
-                    boolean acabou = false;
-                    int geracao = 0;
-
-                    while (!acabou) {
-
-                        String acabar = "";
-
-
-                        System.out.printf("Número de indivíduos da %dª geração\n",geracao+1);
-                        nIndividuos[geracao] = read.nextInt();
-=======
-                    if(flag==0){
->>>>>>> 6aaca8b0a7b2eb018d2df493760908e43eaf7d3b
-
-                        Welcome.Welcome();
-                        flag++;
-                        System.out.printf("Por favor introduza a matriz de Lelie:\n\n");
-
-                    }
-
-                    else if(flag>0)  {
-
-                        System.out.printf("\n\n\n\nPor favor introduza novamente a matriz de Lelie:\n\n");
->>>>>>> 3fb32a80422f9eb5246d376c7fb9fe5bc326ee57
-
-        if (args.length != 0) {
-
-            try {
-
-                // Interface.main(args);
-
-<<<<<<< HEAD
             } catch (Exception e) {
 
                 e.printStackTrace();
@@ -169,39 +112,8 @@ public class Main {
     }
 
 }
-=======
-                    double[][] matriz = Ler.criaMatriz(nIndividuos, taxaS, taxaF, geracao);
 
-                    //Interface.ImprimirMatriz(matriz);
-                    nIndividuos = Ler.limpaVetor(nIndividuos, geracao);
 
-                    System.out.printf("k=%d\n\n",t);
 
-                    System.out.printf("Matriz de Leslie\n");
 
-                    Apresentar.apresentarMatriz(matriz);
 
-                    Distribuição.ApresentarDist(matriz,t,nIndividuos);
-
-                    read.nextLine();
-
-                    while (!choi.equals("S") && !choi.equals("N")) {
-
-                        System.out.printf("Deseja voltar a introduzir dados?(S/N)\n\n");
-                        choi = read.nextLine();
-
-                        if (choi.equals("S")) {
-
-                            choice = true;
-
-                        } else if (choi.equals("N")) {
-
-                            choice = false;
-
-                        }
-                    }
-                }
-            }
-        }
-    }
->>>>>>> 3fb32a80422f9eb5246d376c7fb9fe5bc326ee57
