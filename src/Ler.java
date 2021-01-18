6aaca8b0a7b2eb018d2df493760908e43eaf7d3b
AzureAD\MiguelJordão(1201487 <1201487@isep.ipp.pt>
2021-01-18 10:10:41 +0000

    Update ( nada de mais )

--- src/Ler.java
+++ src/Ler.java
@@ -1,6 +1,9 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Ler {
+
+    static Scanner read = new Scanner(System.in);
 
     public static double[][] criaMatriz(double[] nIndividuos, double[] taxaS, double[] taxaF, int geracao) {
 
@@ -56,6 +59,63 @@ public class Ler {
 
         return end;
 
+    }
+
+    public static int leslie(double[] nIndividuos,double[]taxaF,double[]taxaS){
+
+        int geracao=0;
+        boolean acabou = false;
+
+        while (!acabou) {
+
+            String acabar = "";
+
+
+            System.out.printf("Número de indivíduos da %dª geração\n\n",geracao+1);
+            nIndividuos[geracao] = read.nextInt();
+
+
+            System.out.printf("\nTaxa de fecundidade da %dª geração\n",geracao+1);
+            System.out.println();
+            taxaF[geracao] = read.nextDouble();
+
+
+            System.out.printf("\nTaxa de mortalidade da %dª geração ",geracao+1);
+            System.out.print("(Se for ultima geração este valor não vai ser lido, por isso pode deixar a 0)\n");
+            System.out.println();
+            taxaS[geracao] = read.nextDouble();
+
+            while (taxaS[geracao] > 1 || 0 > taxaS[geracao]) {
+
+                System.out.printf("\nValor inválido, por favor introduza um valor entre 0 e 1\n\n");
+                taxaS[geracao] = read.nextDouble();
+
+            }
+
+            read.nextLine();
+
+
+            while (!acabar.equals("S") && !acabar.equals("N")) {
+
+                System.out.printf("\nDeseja  introduzir mais dados?(S/N)\n");
+                acabar = read.nextLine();
+
+                if (acabar.equals("N")) {
+
+                    acabou = true;
+
+                } else if (acabar.equals("S")) {
+
+                    acabou = false;
+
+                }
+
+            }
+
+            geracao++;
+
+        }
+        return geracao;
     }
 
 }
