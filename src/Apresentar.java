54cfe332f7e5d9c3e5ea39bd055ccfe0d6b232fc
José Carlos Pereira <1201627@isep.ipp.pt>
2021-01-21 18:57:25 +0000

    Criação de 3 classes para guardar dentro de um ficheiro.

--- src/Apresentar.java
+++ src/Apresentar.java
@@ -100,5 +100,83 @@ public class Apresentar {
         return igual;
 
     }
+     public static void apresentarVetorFicheiro(double[] vetor) {
 
+        for (int j = 0; j < vetor.length; j++) {
+
+            if (j == vetor.length) {
+                String aux = "";
+              aux = "%.2f)"+Double.toString(vetor[j]);
+                System.out.printf("saida.txt",aux );
+            }
+
+            else if (j == 0) {
+                String aux = "";
+                aux = "(%.2f"+Double.toString(vetor[j]);
+                System.out.printf("saida.txt", aux);
+            }
+            else{
+                String aux = "";
+                aux = ";%.2f"+Double.toString(vetor[j]);
+                System.out.printf("saida.txt", aux);
+            }
+
+        }
+
+        Grava.UpdateFicheiro("saida.txt",")");
+
+        Grava.UpdateFicheiro("saida.txt","\n");;
+
+        Grava.UpdateFicheiro("saida.txt","\n");;
+
+    }
+    public static void apresentarTaxaOUDimensãoFicheiro(double[] vetor) {
+
+        int t = 0;
+        String aux= "";
+
+        for (int j = 0; j < vetor.length; j++) {
+
+            if (vetor[j] != 0) {
+        aux = "(%d ; %.2f)" + t + Double.toString(vetor[j]);
+                Grava.UpdateFicheiro("saida.txt",aux);
+                t++;
+
+            }
+
+        }
+
+        Grava.UpdateFicheiro("saida.txt","\n");;
+
+    }
+     public static void apresentarDistribuiçãoFicheiro(double[][] Distribuição) {
+
+        int t = 0;
+        int count2 = 0;
+        String aux = "";
+        for (int i = 0; i < Distribuição.length; i++) {
+
+            for (int j = 0; j < Distribuição[i].length; j++) {
+
+                if (Distribuição[i].length > 0 && Distribuição[i][j] != 0) {
+
+                    if (j == 0) {
+                         aux = "(%d ; %.2f "+ t + Double.toString(Distribuição[i][j]);
+                        Grava.UpdateFicheiro("saida.txt",aux);
+                        t++;
+
+                    } else{
+                        aux = "; %.2f "+ Double.toString(Distribuição[i][j]);
+                        Grava.UpdateFicheiro("saida.txt",aux);
+                    }
+
+                }
+            }
+            if(Distribuição[i][0]!=0){
+
+                Grava.UpdateFicheiro("saida.txt",")\n");;
+            }
+        }
+
+    }
 }
