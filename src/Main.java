0ae430f73579594eb7927a33501fab5f74762e22
Eduardo Sousa <1200920@isep.ipp.pt>
2021-01-22 12:04:45 +0000

    passagem de main para Jordão

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,16 @@ import java.nio.file.Paths;
 public class Main {
 
     public static void main(String[] args) throws InterruptedException, IOException {
-        boolean fich = false;
+
         Path path = Paths.get("Output\\");
         Files.createDirectories(path);
+
+
         String nomeFicheiro ="";
         int f=1;
 
 
         if(args[1].equals("-n")){
-            fich=true;
             nomeFicheiro = args[2];
             f=0;
         }
@@ -25,7 +26,7 @@ public class Main {
 
         } else {
 
-            Menu.Menu(fich,nomeFicheiro,f);
+            Menu.Menu(nomeFicheiro,f);
 
         }
 
